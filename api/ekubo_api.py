9a7897416888df7f285544e08a7c3d1095bbbdea
ManhHung1501 <hungnm2@ftech.ai>
2025-01-15 11:16:19 +0700

    update

--- api/ekubo_api.py
+++ api/ekubo_api.py
@@ -35,22 +35,20 @@ def get_data(
     
     try:
         query = f"""
-        SELECT pair, vol_24h, txn_24h, report_date
+        SELECT pair, vol_24h, txn_24h
         FROM {clickhouse_db}.top_txn_token_report
         WHERE report_date BETWEEN '{from_date}' AND '{to_date}'
         ORDER BY vol_24h DESC
         """
         result = client.execute(query)
 
-        columns = ["pair", "vol_24h", "txn_24h", "report_date"]
+        columns = ["pair", "vol_24h", "txn_24h"]
         formatted_result = [dict(zip(columns, row)) for row in result]
         
         if not formatted_result:
             return {"message": "No data found for the given date range"}
         
-        return {"data": formatted_result}
-        if not result:
-            return {"message": "No data found for the given date range"}
-        return {"data": result}
+        return {"top_tokens": formatted_result}
+       
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
